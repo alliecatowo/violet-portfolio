7a9f0387ca63bd2148fd6236be145448f1866f7e
alliecatowo <98134425+alliecatowo@users.noreply.github.com>
2025-10-29 21:48:00 +0000

    Deploying to gh-pages from @ alliecatowo/violet-portfolio@ee3a06d65a79403eacd1eff46af6ac0a2daeb151 🚀

--- _nuxt/BYPL1zim.js
+++
@@ -1,1 +0,0 @@
-import{y as r,Q as c,af as s,c as l,d as p,C as u,n as f}from"#entry";const i={base:""},g={__name:"ProseStrong",props:{class:{type:null,required:!1}},setup(e){const a=e,o=r(),t=c(()=>s({extend:s(i),...o.ui?.prose?.strong||{}}));return(n,m)=>(p(),l("strong",{class:f(t.value({class:a.class}))},[u(n.$slots,"default")],2))}};export{g as default};
